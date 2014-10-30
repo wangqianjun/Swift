da2025b214a1b1c4503f440756dd0d517317a45f
王钱钧 <arthur.wang@yoho.cn>
2014-10-30 11:52:10 +0800

    Swift学习Demo
    学习Swift过程中，写的demo，方便以后查阅

---
+++ Swift-基础部分/Swift-基础部分/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swift-基础部分
+//
+//  Created by 王钱钧 on 14/10/30.
+//  Copyright (c) 2014年 王钱钧. All rights reserved.
+//
+
+import UIKit
+
+let constant: String = "常量"
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
